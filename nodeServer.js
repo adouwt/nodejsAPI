05fa067dbd2ce6655a0d0d3e553ab5eeeef74d5a
wangtao02 <wangt02@zendaimoney.com>
2019-03-13 20:59:28 +0800

    test

--- nodeServer.js
+++ nodeServer.js
@@ -1,36 +1,37 @@
-import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
-import logger from './core/logger/app-logger'
-import morgan from 'morgan'
-import config from './core/config/config.dev'
-import { getRouter, postRouter} from './routes/indexRouter.js'
-import connectToDb from './db/connect'
+import express from "express"
+import bodyParser from "body-parser"
+import cors from "cors"
+import logger from "./core/logger/app-logger"
+import morgan from "morgan"
+import config from "./core/config/config.dev"
+import { getRouter, postRouter } from "./routes/indexRouter.js"
+import connectToDb from "./db/connect"
 
-const port = config.serverPort;
+const port = config.serverPort
 logger.stream = {
-    write: function(message, encoding){
-        logger.info(message);
-    }
-};
+  write: function(message, encoding) {
+    logger.info(message)
+  }
+}
 
-connectToDb();
+connectToDb()
 
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan("dev", { "stream": logger.stream }));
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(morgan("dev", { stream: logger.stream }))
 
 // 挂在路由 /get 是一级路由  转到 ./routes/* 中来 里面的 /* 是子路由，完整的路由 /get/*
-app.use('/get', getRouter);
-app.use('/post', postRouter);
+app.use("/get", getRouter)
+app.use("/post", postRouter)
 
 //Index route
-app.get('/', (req, res) => {
-    res.send('this is a index');
-});
+app.get("/", (req, res) => {
+  res.send("this is a index,服务已经开启！")
+})
 
 app.listen(port, () => {
-    logger.info('server started - ', port);
-});
+  logger.info("server started - ", port)
+  logger.info("项目已经启动：", `http://localhost:${port}`)
+})
